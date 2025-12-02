c861753b71cf3229f848cb40acc22de5a1c2eabe
bobkim67 <144781896+bobkim67@users.noreply.github.com>
2025-12-02 16:30:05 +0900

    Update Dashboard_v11.py
    fixed a counting logic for retired employee
    line 730 ~ 733

--- Dashboard_v11.py
+++ Dashboard_v11.py
@@ -728,8 +728,10 @@ def simul(명부, 기준일, sim_yr, 할인율, bu_i):
         'DUR', '보정계수', '평가방법', '정년', '할당', '임직원', 'bu', '승급률', '퇴직률', '사망률', '할인율'
     ])
 
-    retired.append((result_table['정년'] <= result_table['시산연령']).sum())
-    #result_table = result_table[result_table['정년'] >= result_table['시산연령']]
+    current_retired = (result_table['정년'] <= result_table['시산연령']).sum()
+    new_retired = current_retired - (retired_cumulative if sim_yr > 0 else 0)
+    retired.append(new_retired)
+          
     명부 = 명부[명부['사원번호'].isin(result_table['사번'])]
     
     return result_table, 명부, cf_data
@@ -3030,3 +3032,4 @@ if all([명부 is not None, 기초율 is not None, 지급률 is not None, macro 
                 ax.set_xlabel("Cumulative Return(%)")
                 ax.set_ylabel("Frequency")
                 st.pyplot(fig)
+
